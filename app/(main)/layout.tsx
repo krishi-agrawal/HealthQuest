d5e43ff23b6f2f8fe73d0c03018274a941ddf328
Krishi Agrawal <krishi.agrawal26@gmail.com>
2024-09-04 23:48:28 +0530

    minor changes

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -8,7 +8,7 @@ type Props = {
 const MainLayout = ({ children }: Props) => {
   return (
     <>
-    <MobileHeader />
+      <MobileHeader />
       <Sidebar className="hidden lg:flex" />
       <main className="lg:pl-[256px] h-full pt-[50px] lg:pt-0">
         <div className="h-full bg-red-500">{children}</div>
